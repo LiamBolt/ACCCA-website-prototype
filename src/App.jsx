d0d313f09e36cac28534f1c222e1489a3866004f
LiamBolt <amwineliam@gmail.com>
2025-04-24 06:39:04 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-import React from "react";
-
-function Test() {
-  return (
-    <h2 className="bg-blue-500 text-2xl font-medium">This is my new website</h2>
-  );
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Activities from './pages/activities';
+import Tours from './pages/tours';
+import Partners from './pages/partners';
+import Testimonials from './pages/testimonials';
+import Shop from './pages/shop';
+import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Test/>
-  )
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/tours" element={<Tours />} />
+            <Route path="/partners" element={<Partners />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
